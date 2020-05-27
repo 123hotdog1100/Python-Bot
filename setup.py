82e81ceed7105a51fc3209208f716851fb6cdf0d
BuildTools <unconfigured@null.spigotmc.org>
2020-05-27 23:27:12 +0100

    making status

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ client = commands.Bot(command_prefix = ".")
 
 @client.event
 async def on_ready():
+    await client.change_presence(game=discord.Game(name="Being fecking cool"))
     print('bot is Ready.')
 
 @client.command()
