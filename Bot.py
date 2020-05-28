62f135a85300b2b56068e577f7319907282e2bbf
BuildTools <unconfigured@null.spigotmc.org>
2020-05-28 13:46:10 +0100

    Added status

--- Bot.py
+++ Bot.py
@@ -2,48 +2,67 @@ import discord
 from discord.ext import commands
 from discord.voice_client import VoiceClient
 import random
-client = commands.Bot(command_prefix = ".")
+
+client = commands.Bot(command_prefix=".")
 
 
 @client.event
 async def on_ready():
+    await client.change_presence(activity=discord.Game('My prefix is', '.'))
     print('bot is Ready.')
+
 
 @client.command()
 async def ping(ctx):
-    await ctx.send(f"pong! {round(client.latency *1000)} MS")
+    await ctx.send(f"pong! {round(client.latency * 1000)} MS")
 
 
 @client.command(aliases=['8ball', 'eightball'])
-async def _8ball(ctx,* , Question):
-    responses =["It is certain.",
-                "It is decidedly so.",
-                "Without a doubt.",
-                "Yes - definitely.",
-                "You may rely on it.",
-                "As I see it, yes.",
-                "Most likely.",
-                "Outlook good.",
-                "Yes.",
-                "Signs point to yes.",
-                "Reply hazy, try again.",
-                "Ask again later.",
-                "Better not tell you now.",
-                "Cannot predict now.",
-                "Concentrate and ask again.",
-                "Don't count on it.",
-                "My reply is no.",
-                "My sources say no.",
-                "Outlook not so good.",
-                "Very doubtful."]
+async def _8ball(ctx, *, Question):
+    responses = ["It is certain.",
+                 "It is decidedly so.",
+                 "Without a doubt.",
+                 "Yes - definitely.",
+                 "You may rely on it.",
+                 "As I see it, yes.",
+                 "Most likely.",
+                 "Outlook good.",
+                 "Yes.",
+                 "Signs point to yes.",
+                 "Reply hazy, try again.",
+                 "Ask again later.",
+                 "Better not tell you now.",
+                 "Cannot predict now.",
+                 "Concentrate and ask again.",
+                 "Don't count on it.",
+                 "My reply is no.",
+                 "My sources say no.",
+                 "Outlook not so good.",
+                 "Very doubtful."]
     await ctx.send(f'Question: {Question}\nAnswer:{random.choice(responses)}')
+
 
 @client.command()
 async def join(ctx):
     channel = ctx.author.voice.channel
     await channel.connect()
 
+
 @client.command()
 async def leave(ctx):
     await ctx.voice_client.disconnect()
+
+
+@join.error
+async def join_error(ctx, error):
+    if isinstance(error, commands.CommandInvokeError):
+        await ctx.send('You need to be in a voice chat for this')
+
+
+@leave.error
+async def leave_error(ctx, error):
+    if isinstance(error, commands.CommandInvokeError):
+        await ctx.send('I\'m not currently in a voice chat')
+
+
 client.run('NzE0OTU3NDQ4NTUyNjQ0NjE4.Xs7omQ.Gl1S8bo_0rmNjdoY7iWgndwmpS4')
